5ce6a5d51db06d2bb5b90abdca942120adf10983
skyjessica7609 <skyjessica7609@gmail.com>
2026-02-02 11:17:33 +0800

    Update sample-python.py

--- sample-python.py
+++ sample-python.py
@@ -1,3 +1,30 @@
 print('hihi')
 # this is single line comment
 # BMI
+# Weight Height
+'''
+multi line comment
+'''
+# BMI = weight / (height * height)
+# string is text, we enclose using quote
+# 'this is a string'
+# 'this is also a string'
+# basic data types
+height = float(input("Enter height in metres")) #float - decimal values
+weight = float(input("Enter weight kg:")) #integers - whole numbers
+
+bmi = weight / (height * height)
+#str(variable)
+# str - function to convert numbers to string values
+print('Your BMI is'+ str(bmi))
+# string concatenation
+quote = "Monday is Bluey" #string variable
+print( "Today is" + quote) #quote is a string
+quote= "Monday is actually bad..."
+print("Today is" + quote) #quote is a string
+#basic data types in python
+a = 10 #integers -> whole numbers
+b =3.14 #floats -> decimal values
+c= True #booleans -> True or False \Yes or No\
+d = "this is a string" #string => text values
+e = "this is a string"
